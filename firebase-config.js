2e59baa6095a08555dd4958cbddfe1556ab54c75
Harry Lee <proxsolver@gmail.com>
2025-08-02 22:51:18 +0900

    update firebase config with real setting and fix gitignor

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC6bvEaHsxpLtPv5zM99PmgAwOnRXnhkBM",
+    authDomain: "habit-dc62a.firebaseapp.com",
+    projectId: "habit-dc62a",
+    storageBucket: "habit-dc62a.firebasestorage.app",
+    messagingSenderId: "668374525477",
+    appId: "1:668374525477:web:ecbecd95ec631fb82cc1cc",
+    measurementId: "G-DV5Z9YVTZG"
+  };
