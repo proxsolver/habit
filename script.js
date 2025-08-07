4d00df7c99ab87ebc9f3f28616478e28fd245116
Harry Lee <proxsolver@gmail.com>
2025-08-07 23:35:35 +0900

    debug(script): Add debugger statement to inspect modal state on show

--- script.js
+++ script.js
@@ -1217,6 +1217,8 @@ async function showDetailStatsModal(routineId) {
     contentEl.style.display = 'none';
     modal.style.display = 'flex';
 
+    debugger; 
+
     const routine = sampleRoutines.find(r => r.id === routineId);
     titleEl.textContent = `"${routine.name}" 상세 통계`;
     
